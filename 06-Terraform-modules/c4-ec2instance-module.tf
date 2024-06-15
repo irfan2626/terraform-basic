4434f6a5c890c7b7f3e7ca1886608aeaf5c37391
irfan2626 <121598565+irfan2626@users.noreply.github.com>
2024-06-15 08:24:23 +0530

    Update c4-ec2instance-module.tf

--- 06-Terraform-modules/c4-ec2instance-module.tf
+++ 06-Terraform-modules/c4-ec2instance-module.tf
@@ -7,8 +7,8 @@ module "ec2_cluster" {
   instance_count         = 2
 
   ami                    = data.aws_ami.amzlinux.id 
-  instance_type          = "t2.micro"
-  key_name               = "terraform-key"
+  instance_type          = "t3.medium"
+  key_name               = "project"
   monitoring             = true
   vpc_security_group_ids = ["sg-b8406afc"] # Get Default VPC Security Group ID and replace
   subnet_id              = "subnet-4ee95470" # Get one public subnet id from default vpc and replace
