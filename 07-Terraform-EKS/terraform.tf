6abe6309ccef652cfed3cfb4ce122b067dc5a833
irfan2626 <121598565+irfan2626@users.noreply.github.com>
2024-06-15 08:47:49 +0530

    Update terraform.tf

--- 07-Terraform-EKS/terraform.tf
+++ 07-Terraform-EKS/terraform.tf
@@ -1,24 +1,23 @@
 terraform {
-
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.7.0"
+      source = "hashicorp/aws"
+      version = "5.54.1"
     }
 
     random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      source = "hashicorp/random"
+      version = "3.6.2"
     }
 
     tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      source = "hashicorp/tls"
+      version = "4.0.5"
     }
 
     cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      source = "hashicorp/cloudinit"
+      version = "2.3.4"
     }
   }
 }
